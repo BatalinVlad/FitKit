1d8177390df691c52760a4e0e5c39be5a79547be
BatalinVlad <batalinvlad@gmail.com>
2023-03-12 19:48:18 +0200

    adding socket env

--- backend/app.js
+++ backend/app.js
@@ -21,7 +21,7 @@ app.use(cors());
 // const port = process.env.PORT || 2400;
 
 const server = http.createServer(app);
-server.listen(2400, () => {
+server.listen(3001, () => {
   console.log('SERVER RUNNING');
 })
 
