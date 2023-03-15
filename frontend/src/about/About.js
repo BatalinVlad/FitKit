dd49171a2be2de6bec61e3154dcaf9fba0ddc732
BatalinVlad <batalinvlad@gmail.com>
2023-03-15 12:16:02 +0200

    adding about page content

--- frontend/src/about/About.js
+++ frontend/src/about/About.js
@@ -1,8 +1,39 @@
 import React from 'react';
+import { AiOutlineLinkedin } from 'react-icons/ai';
+import { SiGmail } from 'react-icons/si';
+import { TfiInstagram } from 'react-icons/tfi';
+
+import './About.css'
 
 const About = () => {
-
-    return <h1> about page </h1>
+    return (
+        <div className='about-page-container'>
+            <div className='about-title flex column text-center'>
+                <h1> HEY, I'M VLAD </h1>
+                <p>A Fitness trainer and a FullStack programer.</p>
+            </div>
+            <div className='about-container flex fill justify-center'>
+                <div className='about-trainer flex column text-center align-center space-between'>
+                    <p>2 years ago i stated to train... bla bla bla</p>
+                    <div className='about-trainer-image'>
+                    </div>
+                </div>
+                <div className='about-programer flex column text-center align-center space-between'>
+                    <p>I love code, creating stuff and study new skills... bla bla bla</p>
+                    <div className='about-programer-image'>
+                    </div>
+                </div>
+            </div>
+            <div className='about-page-footer fill text-center'>
+                <h1>CONTACT ME</h1>
+                <div className='contact-me-icons'>
+                    <AiOutlineLinkedin />
+                    <SiGmail />
+                    <TfiInstagram />
+                </div>
+            </div>
+        </div>
+    )
 };
 
 export default About;
