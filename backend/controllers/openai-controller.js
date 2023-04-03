ef3950734407587a22f5d9f56d5eb1c616460b9c
BatalinVlad <batalinvlad@gmail.com>
2023-04-03 19:25:19 +0300

    fixing cors

--- backend/controllers/openai-controller.js
+++ backend/controllers/openai-controller.js
@@ -1,64 +1,64 @@
 
-const { Configuration, OpenAIApi } = require('openai');
-// const HttpError = require('../models/http-error');
-
-const createPlans = ('/cors', async (req, res, next) => {
-
-    const configuration = new Configuration({
-        organization: process.env.ORGANIZATION,
-        apiKey: process.env.OPENAI_API_KEY,
-    });
-    const openai = new OpenAIApi(configuration);
-
-    const { message } = req.body;
-    const completion = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: message,
-        max_tokens: 1000,
-        temperature: 0
-
-
-    });
-    res.set('Access-Control-Allow-Origin', '*');
-
-    res.json({
-        completion: completion.data.choices[0].text
-    })
-});
-
-
-exports.createPlans = createPlans;
-
-
-
-// const cors = require('cors')({ origin: true });
 // const { Configuration, OpenAIApi } = require('openai');
-// const HttpError = require('../models/http-error');
+// // const HttpError = require('../models/http-error');
 
-// const createPlans = functions.https.onRequest((req, res) => {
-//     cors(req, res, async () => {
-//         const configuration = new Configuration({
-//             organization: process.env.ORGANIZATION,
-//             apiKey: process.env.OPENAI_API_KEY,
-//         });
-//         const openai = new OpenAIApi(configuration);
-//         try {
-//             const { message } = req.body;
-//             const completion = await openai.createCompletion({
-//                 model: "text-davinci-003",
-//                 prompt: message,
-//                 max_tokens: 1000,
-//                 temperature: 0
+// const createPlans = ('/cors', async (req, res, next) => {
+
+//     const configuration = new Configuration({
+//         organization: process.env.ORGANIZATION,
+//         apiKey: process.env.OPENAI_API_KEY,
+//     });
+//     const openai = new OpenAIApi(configuration);
+
+//     const { message } = req.body;
+//     const completion = await openai.createCompletion({
+//         model: "text-davinci-003",
+//         prompt: message,
+//         max_tokens: 1000,
+//         temperature: 0
 
 
-//             });
-//             res.json({ completion: completion.data.choices[0].text });
-//         } catch (err) {
-//             const error = new HttpError('something went wrong, try again later', 500)
-//             return next(error);
-//         }
+//     });
+//     res.set('Access-Control-Allow-Origin', '*');
+
+//     res.json({
+//         completion: completion.data.choices[0].text
 //     })
 // });
 
 
 // exports.createPlans = createPlans;
+
+
+
+const cors = require('cors')({ origin: true });
+const { Configuration, OpenAIApi } = require('openai');
+const HttpError = require('../models/http-error');
+
+const createPlans = functions.https.onRequest((req, res) => {
+    cors(req, res, async () => {
+        const configuration = new Configuration({
+            organization: process.env.ORGANIZATION,
+            apiKey: process.env.OPENAI_API_KEY,
+        });
+        const openai = new OpenAIApi(configuration);
+        try {
+            const { message } = req.body;
+            const completion = await openai.createCompletion({
+                model: "text-davinci-003",
+                prompt: message,
+                max_tokens: 1000,
+                temperature: 0
+
+
+            });
+            res.json({ completion: completion.data.choices[0].text });
+        } catch (err) {
+            const error = new HttpError('something went wrong, try again later', 500)
+            return next(error);
+        }
+    })
+});
+
+
+exports.createPlans = createPlans;
