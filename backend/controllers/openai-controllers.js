f71099c62dc8483930061b7db7dda379ec482e71
BatalinVlad <batalinvlad@gmail.com>
2023-04-11 18:42:45 +0300

    fixing cors

--- backend/controllers/openai-controllers.js
+++ backend/controllers/openai-controllers.js
@@ -1,14 +1,14 @@
 
-const functions = require('firebase-functions');
 const { Configuration, OpenAIApi } = require('openai');
 const HttpError = require('../models/http-error');
 
-const createPlans = functions.https.onRequest(async (req, res, next) => {
-    console.log('sending req')
+const createPlans = async (req, res, next) => {
+
     const configuration = new Configuration({
         organization: process.env.ORGANIZATION,
         apiKey: process.env.OPENAI_API_KEY,
     });
+
     const openai = new OpenAIApi(configuration);
     try {
         const { message } = req.body;
@@ -23,8 +23,7 @@ const createPlans = functions.https.onRequest(async (req, res, next) => {
         const error = new HttpError('something went wrong, try again later', 500)
         return next(error);
     }
-})
-    ;
+};
 
 
 exports.createPlans = createPlans;
