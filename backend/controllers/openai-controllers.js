bf48fb8055f0b2c6be80d6b59bd736eb7a5b8e3f
BatalinVlad <batalinvlad@gmail.com>
2023-06-17 23:06:50 +0300

    trying to solve post 500

--- backend/controllers/openai-controllers.js
+++ backend/controllers/openai-controllers.js
@@ -5,27 +5,28 @@ const HttpError = require('../models/http-error');
 const createPlans = async (req, res, next) => {
     console.log('sending ai req');
 
-    const configuration = new Configuration({
-        organization: process.env.ORGANIZATION,
-        apiKey: process.env.OPENAI_API_KEY,
-    });
+    // const configuration = new Configuration({
+    //     organization: process.env.ORGANIZATION,
+    //     apiKey: process.env.OPENAI_API_KEY,
+    // });
 
-    const openai = new OpenAIApi(configuration);
+    // const openai = new OpenAIApi(configuration);
     const { message } = req.body;
+    res.json({ completion: message });
 
 
-    try {
-        const completion = await openai.createCompletion({
-            model: "text-davinci-003",
-            prompt: message,
-            max_tokens: 3000,
-            temperature: 0
-        });
-        res.json({ completion: completion.data.choices[0].text });
-    } catch (err) {
-        const error = new HttpError('something went wrong, try again later', 500)
-        return next(error);
-    }
+    // try {
+    //     const completion = await openai.createCompletion({
+    //         model: "text-davinci-003",
+    //         prompt: message,
+    //         max_tokens: 3000,
+    //         temperature: 0
+    //     });
+    //     res.json({ completion: completion.data.choices[0].text });
+    // } catch (err) {
+    //     const error = new HttpError('something went wrong, try again later', 500)
+    //     return next(error);
+    // }
 };
 
 
