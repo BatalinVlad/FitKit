ef78efc6bc3e0d110671c9f09af4fce37789b9ee
BatalinVlad <batalinvlad@gmail.com>
2023-07-05 15:25:37 +0300

    adding styles to product page

--- frontend/src/products/pages/ProductPage.js
+++ frontend/src/products/pages/ProductPage.js
@@ -2,8 +2,10 @@ import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import MainNavigation from '../../shared/components/Navigation/MainNavigation';
-// import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import Button from '../../shared/components/FormElements/Button';
+import { BsCartCheck } from 'react-icons/bs';
+import { TbJewishStar } from 'react-icons/tb';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { getProductById } from '../../shared/features/product/ProductSlice';
@@ -12,7 +14,7 @@ const ProductPage = () => {
     const dispatch = useDispatch();
     const productId = useParams().productId;
     const product = useSelector((state) => state.products.selectedProduct);
-    console.log(product);
+    const isLoading = useSelector((state) => state.products.isLoading);
 
     useEffect(() => {
         dispatch(getProductById(productId));
@@ -23,23 +25,19 @@ const ProductPage = () => {
             {product &&
                 <div className='product-page flex column 100vh'>
                     <MainNavigation />
-                    {/* {isLoading && <LoadingSpinner asOverlay />} */}
+                    {isLoading && <LoadingSpinner asOverlay />}
                     <div className='product-container-wrapper flex column center'>
-                        <div className='fill-width flex space-between'>
-                            <p className='ml10'> {product.price} $ </p>
-
-                            <p className='mr10'>{product.date}</p>
-                        </div>
-                        <div className='flex row-rev'>
-                            <div className='flex column'>
+                            <p className='fill-width text-start ml10'> {product.date} </p>
+                        <div className='section-one flex'>
+                            <div className='actions flex column'>
                                 <div className='product__buy-now center'>
                                     <Button type='link' to='/products' regularAction>
-                                        BUY NOW
+                                        <BsCartCheck />
                                     </Button>
                                 </div>
                                 <div className='product__add-to-fav center'>
                                     <Button type='link' to='/products' regularAction>
-                                        ADD TO FAV
+                                        <TbJewishStar />
                                     </Button>
                                 </div>
                                 <div className='product__all-products center'>
@@ -48,24 +46,26 @@ const ProductPage = () => {
                                     </Button>
                                 </div>
                             </div>
-                            <div className='product-container flex column'>
+                            <div className='product-container flex'>
                                 <div className='product-container__content'>
-                                    <h1>
-                                        {product.title}
-                                    </h1>
-                                    <h2>
-                                        {product.description}
-                                    </h2>
-                                    <h2>
-                                        {product.description_short}
-                                    </h2>
-                                    <p>
-                                        {product.dietContent}
-                                    </p>
+                                    <div>
+                                        <h1>
+                                            {product.title}
+                                        </h1>
+                                        <h2>
+                                            {product.description}
+                                        </h2>
+                                        <h2>
+                                            {product.description_short}
+                                        </h2>
+                                        <p className='diet-content mt10'>
+                                            {product.dietContent}
+                                        </p>
+                                    </div>
+                                    <p className='bold mt10 fs16'> {product.price} $ </p>
+
                                 </div>
-                                {/* <div> */}
-                                <img src={`${product.image.secure_url}`} alt={product.title} className="img-cover fill" />
-                                {/* </div> */}
+                                <img src={`${product.image.secure_url}`} alt={product.title} className="img-cover" />
                             </div>
                         </div>
                     </div>
